76c5c3b87f153e59363c4be45ad7bceeca072f8b
h1k0r <62479366+h1ck0r@users.noreply.github.com>
2022-08-28 17:35:43 +0800

    Update Copyrights.vue

--- src/components/Copyrights.vue
+++ src/components/Copyrights.vue
@@ -3,7 +3,7 @@
         <v-row justify="center" no-gutters>
             <v-col style="font-size: 14px;" class="py-4 text-center white--text" cols="12">
                 {{ new Date().getFullYear() }} — <strong>Powered by
-                <a class="white--text" href="https://cardinal.ink/" target="_blank">Cardinal</a>
+                <a class="white--text" href="https://github.com/h1ck0r/" target="_blank">h1ck0r</a>
             </strong>
             </v-col>
         </v-row>
